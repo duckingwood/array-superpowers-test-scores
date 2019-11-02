d1b52881280e0d28a12d400cca3f206cff16b7f6
duckingwood <duckingwood@gmail.com>
2019-11-02 18:10:25 -0500

    Add Todo list for Project

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 const getRandomIntInclusive = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
   
-  // Generate a Random array with 50 random numbers ranging from 0 to 100.
-  const randomTestScores = Array.From({ lenght: 50 }, () =>
+// Generate a Random array with 50 random numbers ranging from 0 to 100.
+const randomTestScores = Array.From({ lenght: 50 }, () =>
     getRandomIntInclusive(50, 100)
     );
     
     console.log(randomTestScores);
     
-    // Developer's Note: The real work for applying Array Superpowers starts below here!
-    
+// Developer's Note: The real work for applying Array Superpowers starts below here!
+/**
+ * TODO 1. Filter out all scores below a certain threshold
+ * 2. Get the average score
+ * 3. Get Letter grades for the score.
+ * 4. Tally the results based on letter grade
+ */
