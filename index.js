85f379c19817317452bd8886c42a8b9b37104089
duckingwood <duckingwood@gmail.com>
2019-11-02 18:52:48 -0500

    Add getAvg Function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const getRandomIntInclusive = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
   
 // Generate a Random array with 50 random numbers ranging from 0 to 100.
-const randomTestScores = Array.From({ lenght: 50 }, () =>
+const randomTestScores = Array.From({ length: 500 }, () =>
     getRandomIntInclusive(50, 100)
     );
     
@@ -15,7 +15,7 @@ const randomTestScores = Array.From({ lenght: 50 }, () =>
  * 2. Get the average score
  * 3. Get Letter grades for the score.
  * 4. Curve the scores.
- * 4. Tally the results based on letter grade with or without a curve.
+ * 5. Tally the results based on letter grade with or without a curve.
  */
 
  function filterScore(score, threshold = 60) {
@@ -52,8 +52,16 @@ console.log(`Transformer function is: ${transformer.toString()}`);
   return scores.map(score => transformer(score));
 }
 
-// TODO: Filter out the `undefined's.
-const results = transformScores(randomTestScores, filterScore).filter(
-  score => score
-);
+/**
+ * Use reduce to sum up the numbers in an Array and divide by the length of the array.
+ * @param{Array} scores
+ * @return Results after 'summing up' all of the scores and dividing the length.
+ */
+function getAvg(scores) {
+  return scores.reduce((total, score) => (total += score)) / score.length;
+}
+
+const results = getAvg(randomTestScores);
+
 console.log(`Results: ${results}`);
+
