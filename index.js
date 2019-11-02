92dacb2c19ecc7e2a0206c888440515c31384d30
duckingwood <duckingwood@gmail.com>
2019-11-02 18:22:56 -0500

    Update basic formula for test score

--- index.js
+++ index.js
@@ -14,5 +14,38 @@ const randomTestScores = Array.From({ lenght: 50 }, () =>
  * TODO 1. Filter out all scores below a certain threshold
  * 2. Get the average score
  * 3. Get Letter grades for the score.
- * 4. Tally the results based on letter grade
+ * 4. Curve the scores.
+ * 4. Tally the results based on letter grade with or without a curve.
  */
+
+ function filterScore(score, threshold = 60) {
+   if (score < threshold) {
+     return score;
+   }
+ }
+
+console.log(filterScore(50));
+console.log(filterScore(70));
+
+function assignLetterGrade(score) {
+  if (score >= 90) {
+    return "A";
+  }
+  if (score >= 80) {
+    return "B";
+  }
+  if (score >= 70) {
+    return "C";
+  }
+  return "F";
+}
+
+console.log(filterScore(95));
+console.log(filterScore(85));
+console.log(filterScore(75));
+
+function curveScore(score, additionalPoints = 10) {
+  return score + additionalPoints; 
+}
+
+// console.log(curveScore(75));
